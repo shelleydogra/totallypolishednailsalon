7b974144be17d7cdb35e317e8aa72278dd7a8978
Shelley Dogra <shelleycdogra@lewisu.edu>
2018-01-28 15:26:04 -0600

    clock function and css styling in jumbotron

--- js/script.js
+++ js/script.js
@@ -2,4 +2,18 @@
 var pageLoad = function() {
 	prompt("This is totally polished Rates", "ok")
 }
-	
+
+function showClock() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    
+    if(h > 12) { h = h - 12;}
+    if(m < 10) {m = "0" + m;}
+    if(s < 10) {s = "0" + s;}
+    
+    return h + ":" + m + ":" + s;    
+}
+
+setTimeout(showClock(), 1000);
