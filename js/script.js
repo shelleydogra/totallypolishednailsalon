0aa227782472fc8356daf2a7f9e192b07bbcce25
Shelley Dogra <shelleycdogra@lewisu.edu>
2018-01-28 23:27:21 -0600

    Added date and time HH:MM format refreshed every second

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 
-var pageLoad = function() {
-	prompt("This is totally polished Rates", "ok")
-}
 
+
+/*
 function showClock() {
     var date = new Date();
     var h = date.getHours();
@@ -13,7 +12,59 @@ function showClock() {
     if(m < 10) {m = "0" + m;}
     if(s < 10) {s = "0" + s;}
     
-    return h + ":" + m + ":" + s;    
+    var clock = h + ":" + m + ":" + s;   
+    
+    
+    
+    return clock;
+    
+     
+    
 }
+*/
 
-setTimeout(showClock(), 1000);
+(function(){
+'use strict';
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        var c = document.getElementById('current-time');
+        var d = document.getElementById('current-date');
+        
+        setInterval( function updateTime() {
+            var date = new Date();
+            
+            var h = date.getHours();
+            var m = date.getMinutes();
+            var month = date.getMonth();
+            var day = date.getDate();
+            var year = date.getFullYear();           
+            var ampm = 'A.M.'
+            
+            month = formatMonth(month);
+                        
+            if(h > 12) { 
+                h -= 12;
+                ampm = 'P.M.';
+            }else if(h === 0){
+                h = 12;
+            }
+            if(m < 10) {m = "0" + m;}
+            
+            var sep = ':';
+            if (date.getSeconds() % 2 === 1) sep = ' ';
+            
+            c.innerHTML =  h + sep + m + ' ' + ampm;
+            d.textContent = month + ' ' + day + ", " + year;
+        } , 1000);
+        
+        function formatMonth(m) {
+            var monthNames = ['January', 'February', 'March', 'April', 'May', 
+                          'June' , 'July', 'August', 'September', 'October',
+                          'November','December'];
+                          
+            return monthNames[m];
+        }
+});
+    
+    
+})();
