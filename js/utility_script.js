5b8b298e2e881e289ced0d77e9ef870aba2e460e
Shelley Dogra <shelleycdogra@lewisu.edu>
2019-07-11 00:50:02 -0500

    Hours Edit Populated

--- js/utility_script.js
+++ js/utility_script.js
@@ -3,6 +3,61 @@
 
     document.addEventListener('DOMContentLoaded', function() {
 
+        var dayArray = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+        var hoursArray = [ "1", "2", "3", "4", "5", "6","7", "8", "9", "10", "11", "12"];
+        var minArray = ["00", "15", "30", "45"];
+        
+        for (var i = 0; i < dayArray.length; i++) {
+            var day = document.getElementById(dayArray[i])
+            day.childNodes[1].innerHTML = dayArray[i];
+        }
+        
+        for (var i = 0; i < dayArray.length; i++) {
+            for (var j = 0; j < 4; j++) {
+                var h = document.getElementById(dayArray[i]).getElementsByTagName('select')[j];
+               
+                if ( h.id === "hours"){
+                    insertOption (hoursArray, h);
+                } else {
+                    insertOption (minArray, h)
+                }
+            }
+        }
+        
+
+
+
+
+
+
+
+        
+        function insertOption (options, weekDay ) {
+            for (var i = 0; i < options.length; i++) {
+                    var opt = options[i];
+                    var el = document.createElement("option");
+                    el.textContent = opt;
+                    el.value = opt;
+                    weekDay.appendChild(el);
+                }
+        }
+        
+    });
+
+})();
+
+
+
+
+
+
+
+/*
+(function(){
+'use strict';
+
+    document.addEventListener('DOMContentLoaded', function() {
+
         var day = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
         var options = ["7", "8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6"];
         var day_min = ["su", "mo", "tu", "we", "th", "fr", "sa"];
@@ -40,3 +95,4 @@
     });
 
 })();
+*/
