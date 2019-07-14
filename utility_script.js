7a16c1ae3ae26d32c72bba806d67fd699ecc9f0c
Shelley Dogra <shelleycdogra@lewisu.edu>
2019-07-13 19:53:49 -0500

    minute added

--- utility_script.js
+++ utility_script.js
@@ -40,25 +40,26 @@
         }
 
         var request;
-if (window.XMLHttpRequest) {
-	request = new XMLHttpRequest();
-} else {
-	request = new ActiveXObject("Microsoft.XMLHTTP");
-}
-request.open('GET', 'data.json');
-request.onreadystatechange = function() {
-	if ((request.readyState===4) && (request.status===200)) {
-		var items = JSON.parse(request.responseText);
-		var output = '<ul>';
-		for (var key in items) {
-			output += '<li>' + items[key].morning[0].hour + '</li>';
-		}
-		output += '</ul>';
-		document.getElementById('update').innerHTML = output;
-	}
-}
-request.send();
-
+        if (window.XMLHttpRequest) {
+        	request = new XMLHttpRequest();
+        } else {
+        	request = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        
+        request.open('GET', 'data.json');
+        request.onreadystatechange = function() {
+        	if ((request.readyState===4) && (request.status===200)) {
+        		var items = JSON.parse(request.responseText);
+        		var output = '<ul>';
+        		for (var key in items) {
+        			output += '<li>' + items[key].morning[0].hour +':'items[key].morning[0].minute+ + '</li>';
+        		}
+        		output += '</ul>';
+        		document.getElementById('update').innerHTML = output;
+        	}
+        }
+        request.send();
+        
 
 
 
