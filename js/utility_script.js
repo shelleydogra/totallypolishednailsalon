ec6a26027451d6e2c7686738c56836a88792fafb
Shelley Dogra <shelleycdogra@lewisu.edu>
2019-07-13 19:39:08 -0500

    testing json

--- js/utility_script.js
+++ js/utility_script.js
@@ -39,14 +39,25 @@
                 }
         }
 
-        fetch('data.json').then(function (response) {
-            return response.json();
-        }).then(function (obj) {
-            console.log(obj);
-        }).catch(function (error) {
-            console.error(error);
-        });
-
+        var request;
+if (window.XMLHttpRequest) {
+	request = new XMLHttpRequest();
+} else {
+	request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+request.open('GET', 'data.json');
+request.onreadystatechange = function() {
+	if ((request.readyState===4) && (request.status===200)) {
+		var items = JSON.parse(request.responseText);
+		var output = '<ul>';
+		for (var key in items) {
+			output += '<li>' + items[key].morning[0].hour + '</li>';
+		}
+		output += '</ul>';
+		document.getElementById('update').innerHTML = output;
+	}
+}
+request.send();
 
 
 
