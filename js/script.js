d1980e599f2fbfb487bcc7dda890072a53602c9f
Shelley Dogra <shelleycdogra@lewisu.edu>
2019-07-20 16:29:43 -0500

    hours from JSON done

--- js/script.js
+++ js/script.js
@@ -6,17 +6,6 @@
         var c = document.getElementById('current-time');
         var d = document.getElementById('current-date');
         var dy = document.getElementById('current-day');
-        
-        
-/*
-        var sun = document.getElementById('sunday');
-        var mon = document.getElementById('monday');
-        var tue = document.getElementById('tuesday');
-        var wed = document.getElementById('wednesday');
-        var thu = document.getElementById('thursday');
-        var fri = document.getElementById('friday');
-        var sat = document.getElementById('saturday');
-*/
 
         setInterval( function updateTime() {
             var date = new Date();
@@ -48,27 +37,6 @@
             d.innerHTML = month + ' ' + ddate + ", " + year;
             dy.innerHTML = day;
 
-            if(isSummer(monthNumber)) {
-//                 sun.innerHTML = "12:00 - 5:00";
-/*
-                mon.innerHTML = "10:00 - 8:00";
-                tue.innerHTML = "10:00 - 8:00";
-                wed.innerHTML = "10:00 - 8:00";
-                thu.innerHTML = "10:00 - 8:00";
-                fri.innerHTML = "10:00 - 8:00";
-                sat.innerHTML = "10:00 - 6:00";
-*/
-            } else {
-//                 sun.innerHTML = "12:00 - 5:00";
-/*
-                mon.innerHTML = "12:00 - 7:30";
-                tue.innerHTML = "12:00 - 7:30";
-                wed.innerHTML = "12:00 - 7:30";
-                thu.innerHTML = "12:00 - 7:30";
-                fri.innerHTML = "10:00 - 8:00";
-                sat.innerHTML = "10:00 - 6:00";
-*/
-            }
         } , 1000);
 
         function formatMonth(m) {
@@ -96,7 +64,7 @@
         }
         
         
-        
+        // Read & Parse data.json
         var request;
         if (window.XMLHttpRequest) {
         	request = new XMLHttpRequest();
@@ -109,21 +77,15 @@
         	if ((request.readyState===4) && (request.status===200)) {
         		var items = JSON.parse(request.responseText);
         		var keyname = Object.keys(items)
+        		
+        		//populate hours.html hours section from data.JSON
+        		// TODO: pull in the appropriate data for winter/summer hours
         		for (var key in items) {
-        		    
-        		    document.getElementById(key).innerHTML = items[key].morning[0].hour + ':' + items[key].morning[0].minute + ' - ' + items[key].evening[0].hour + ':' + items[key].evening[0].minute
-        		    
-        		    
+        		    document.getElementById(key).innerHTML =    items[key].morning[0].hour + ':' + 
+        		                                                items[key].morning[0].minute + ' - ' + 
+        		                                                items[key].evening[0].hour + ':' + 
+        		                                                items[key].evening[0].minute;
                 }
-        		
-        		
-/*
-        		for (var key in items) {
-        			output += '<li>' + keyname[i] + ' - ' + items[key].morning[0].hour + ':' + items[key].morning[0].minute + ' - ' + items[key].evening[0].hour + ':' + items[key].evening[0].minute + '</li>';
-        			i++;
-        		}
-*/
-
         	}
         }
         request.send();
